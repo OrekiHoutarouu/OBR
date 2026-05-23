9689fc2b526929d3bf3082b9152becb9cec66da2
OrekiHoutarouu <samuelcampelo25@gmail.com>
2026-05-23 14:57:49 +0100

    Creates basic structure for the project

--- computer_vision/main.py
+++ computer_vision/main.py
@@ -7,9 +7,8 @@ def main():
     while True:
         frame = webcam.get_frame(capture)
         frame_rgb = frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-         
-        cv2.imwrite("teste.jpg", frame)
 
+        
         if cv2.waitKey(1) == ord("q"):
             break
 
